2fad796d21326087be375b50c85d81421e09d930
gpu <hussain@technostacks.com>
2023-05-22 18:31:33 +0530

    Task module initiated

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const mongoose = require("mongoose");
 
 app.use(cors());
 app.use(express.json());
-
-// const connect = async () => {
-//   await mongoose
-//     .connect(process.env.MONGO_URI)
-//     //   .then(() => console.log("MongoDB connected..."))
-//     .catch((err) => console.log(err));
-// };
 
 app.get("/", (req, res) => {
     const { name = "World" } = req.query;
@@ -35,8 +27,54 @@ app.post("/user", async (req, res) => {
   res.json(newUser);
 });
 
-// app.listen(3000, () => {
-//   console.log("Server started on port 3000");
-// });
+app.get("/tasks/:userId", async (req, res) => {
+    const { prisma } = require("./parisma");
+    const { userId } = req.params;
+    const tasks = await prisma.task.findMany({
+        where: {
+            authorId: userId
+        }
+    })
+    res.json(tasks)
+})
+
+app.post("/task", async (req, res) => {
+    const { prisma } = require("./parisma");
+    const { title, authorId } = req.body;
+    try{
+        const newTask = await prisma.task.create({
+            data: {
+                title,
+                authorId
+            }
+        })
+        res.status(200).json(newTask)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({error: err})
+    }
+})
+
+app.get("/task/:taskId", async (req, res) => {
+    const { prisma } = require("./parisma");
+    const { taskId } = req.params;
+    const task = await prisma.task.findUnique({
+        where: {
+            id: taskId
+        }
+    })
+    res.json(task)
+})
+
+app.get("/tasks", async (req, res) => {
+    const { prisma } = require("./parisma");
+    const tasks = await prisma.task.findMany()
+    res.json(tasks)
+})
+
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
 
 module.exports = app;
